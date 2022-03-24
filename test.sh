f92c6b25280ba2f72034a9228a411512a0970b63
lijunhao <l@x-cmd.com>
2022-03-24 15:45:03 +0800

    ...

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 f(){
     for i in `seq 1000`; do
-        # ___x_cmd_log http debug hello
-        ___x_cmd_log_logger_func http debug hello
+        ___x_cmd_log http debug hello
+        # ___x_cmd_log_logger_func http debug hello
         # echo hi >/dev/null
         # eval [ $i -gt 1000 ]
         # eval "[ 0 -ge \"\${___X_CMD_LOG_LEVEL_OF_LOGGER_${O}:-1}\" ]"

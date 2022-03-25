cf53c9fae5a39a86601a7329b4f584243cd6bd28
L <l@x-cmd.com>
2022-03-25 13:18:13 +0800

    ...

--- test.sh
+++ test.sh
@@ -2,15 +2,15 @@ g(){
     echo hi >/dev/null
 }
 
-___x_cmd_log init abc
+# ___x_cmd_log init abc
 
 f(){
     for i in `seq 1000`; do
         # ___x_cmd_log :http debug hello
         # ___x_cmd_log_logger_func http debug hello
         # g
-        abc:debug hi 2>/dev/null
-        # echo hi >/dev/null
+        # abc:debug hi 2>/dev/stderr
+        echo hi >/dev/stdout
         # eval [ $i -gt 1000 ]
         # eval "[ 0 -ge \"\${___X_CMD_LOG_LEVEL_OF_LOGGER_${O}:-1}\" ]"
     done
